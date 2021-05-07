0c796bb23bd7019c8657239ea465d21fd910e810
JLLQA <jlewis@qa.com>
2021-05-07 12:37:47 +0100

    PP-22 #comment added option picker for edible

--- HTML + JS/Frontend.js
+++ HTML + JS/Frontend.js
@@ -1,15 +1,16 @@
 const name = document.querySelector("#Name");
 const firstItem = document.querySelector("#firstItem");
 const secondItem = document.querySelector("#secondItem");
+const edible = document.querySelector("#edible");
 const display = document.querySelector("#display");
 
 const getValues = () => {
-    return [String(firstItem.value), String(secondItem.value), String(name.value)];
+    return [String(firstItem.value), String(secondItem.value), String(name.value), String(edible.value)];
 }
 
-const printToHistory = (item1, item2, result) => {
+const printToHistory = (item1, item2, title, edible) => {
     const p = document.createElement("p"); 
-    const text = document.createTextNode(`${result} is ${item1} & ${item2}`);
+    const text = document.createTextNode(`${title} is ${item1} & ${item2} : ${edible}`);
 
     p.appendChild(text);
     display.appendChild(p);
@@ -18,12 +19,13 @@ const printToHistory = (item1, item2, result) => {
 
 // methods 
 const creation = () => {
-    const [first, second, nam] = getValues();
-    printToHistory(first, second, nam);
+    const [first, second, NAME, EDIBLE] = getValues();
+    printToHistory(first, second, NAME, EDIBLE);
 }
 
 
 const resetValues = () => {
+    name.value = "";
     firstItem.value = "";
     secondItem.value = "";
 }
