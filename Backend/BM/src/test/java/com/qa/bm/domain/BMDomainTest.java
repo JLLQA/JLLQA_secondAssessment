7c1ec48af10cbdb8a987d5afbbbc613b6669aa90
JLLQA <jlewis@qa.com>
2021-05-12 07:39:02 +0100

    JL: initial stage of Unit testing

---
+++ Backend/BM/src/test/java/com/qa/bm/domain/BMDomainTest.java
@@ -0,0 +1,13 @@
+package com.qa.bm.domain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BmControllerTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
