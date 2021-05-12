2a4adeb4e7c665c3082ef02fb8379854dd26cb62
JLLQA <jlewis@qa.com>
2021-05-12 07:48:49 +0100

    JL: generated hash and equals for testing

--- Backend/BM/src/main/java/com/qa/bm/domain/BM.java
+++ Backend/BM/src/main/java/com/qa/bm/domain/BM.java
@@ -66,5 +66,55 @@ public class BM {
 	public void setEdible(String edible) {
 		this.edible = edible;
 	}
+	
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((edible == null) ? 0 : edible.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((type1 == null) ? 0 : type1.hashCode());
+		result = prime * result + ((type2 == null) ? 0 : type2.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		BM other = (BM) obj;
+		if (edible == null) {
+			if (other.edible != null)
+				return false;
+		} else if (!edible.equals(other.edible))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (type1 == null) {
+			if (other.type1 != null)
+				return false;
+		} else if (!type1.equals(other.type1))
+			return false;
+		if (type2 == null) {
+			if (other.type2 != null)
+				return false;
+		} else if (!type2.equals(other.type2))
+			return false;
+		return true;
+	}
 
 }
